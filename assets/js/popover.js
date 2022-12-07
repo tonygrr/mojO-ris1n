85166c457d82121fb59cb8f7380605bcb6724f23
tonygrr <89200389413@yandex.ru>
2022-12-07 19:56:28 +0300

    New

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -5,7 +5,7 @@ function htmlToElement(html) {
   return template.content.firstChild
 }
 
-function initPopover(baseURL, useContextualBacklinks, renderLatex) {
+function initPopover(baseURL, useContextualBacklinks) {
   const basePath = baseURL.replace(window.location.origin, "")
   fetchData.then(({ content }) => {
     const links = [...document.getElementsByClassName("internal-link")]
@@ -24,9 +24,16 @@ function initPopover(baseURL, useContextualBacklinks, renderLatex) {
         } else {
           const linkDest = content[li.dataset.src.replace(/\/$/g, "").replace(basePath, "")]
           if (linkDest) {
+            let splitLink = li.href.split("#")
+            let cleanedContent = removeMarkdown(linkDest.content)
+            if (splitLink.length > 1) {
+              let headingName = decodeURIComponent(splitLink[1]).replace(/\-/g, " ")
+              let headingIndex = cleanedContent.toLowerCase().indexOf("<b>" + headingName + "</b>")
+              cleanedContent = cleanedContent.substring(headingIndex, cleanedContent.length)
+            }
             const popoverElement = `<div class="popover">
     <h3>${linkDest.title}</h3>
-    <p>${removeMarkdown(linkDest.content).split(" ", 20).join(" ")}...</p>
+    <p>${cleanedContent.split(" ", 20).join(" ")}...</p>
     <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
 </div>`
             el = htmlToElement(popoverElement)
@@ -35,18 +42,27 @@ function initPopover(baseURL, useContextualBacklinks, renderLatex) {
 
         if (el) {
           li.appendChild(el)
-          if (renderLatex) {
+          if (LATEX_ENABLED) {
             renderMathInElement(el, {
               delimiters: [
                 { left: '$$', right: '$$', display: false },
                 { left: '$', right: '$', display: false },
-                { left: '\\(', right: '\\)', display: false },
-                { left: '\\[', right: '\\]', display: false }
               ],
               throwOnError: false
             })
           }
+
           li.addEventListener("mouseover", () => {
+            // fix tooltip positioning
+            window.FloatingUIDOM.computePosition(li, el, {
+              middleware: [window.FloatingUIDOM.offset(10), window.FloatingUIDOM.inline(), window.FloatingUIDOM.shift()],
+            }).then(({ x, y }) => {
+              Object.assign(el.style, {
+                left: `${x}px`,
+                top: `${y}px`,
+              })
+            })
+
             el.classList.add("visible")
           })
           li.addEventListener("mouseout", () => {
